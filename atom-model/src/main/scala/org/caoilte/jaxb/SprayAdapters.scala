2bd957175340c479815c6995c3cc79091a8613c3
Caoilte O'Connor <caoilte@gmail.com>
2014-09-15 09:01:34 +0100

    a better media type marshaller based on feedback from mailing list

--- atom-model/src/main/scala/org/caoilte/jaxb/SprayAdapters.scala
+++ atom-model/src/main/scala/org/caoilte/jaxb/SprayAdapters.scala
@@ -2,22 +2,31 @@ package org.caoilte.jaxb
 
 import javax.xml.bind.annotation.adapters.XmlAdapter
 
-import spray.http.{MediaTypes, MediaType, Uri}
+import spray.http.parser.HttpParser
+import spray.http._
 
 class UriAdapter extends XmlAdapter[String, Uri] {
   def unmarshal(v: String): Uri = Uri(v)
   def marshal(v: Uri): String = v.toString()
 }
-class UriOptionAdapter extends CustomOptionAdapter[String,Uri](new UriAdapter, null, "")
+class UriOptionAdapter extends CustomOptionAdapter[String, Uri](new UriAdapter, null, "")
 
 class MediaTypeAdapter extends XmlAdapter[String, MediaType] {
-  def unmarshal(v: String): MediaType = {
-    val splitMediaType = v.split("/")
-    if (splitMediaType.length != 2) throw new IllegalArgumentException(s"Invalid media type '$v'")
-    val mediaTypeTuple = (splitMediaType(0), splitMediaType(1))
+  def unmarshal(contentTypeString: String): MediaType = {
 
-    MediaTypes.getForKey(mediaTypeTuple)
-      .getOrElse(throw new IllegalArgumentException(s"Invalid media type '$v'"))
+    // Solution given to me on the Spray User List:
+    // https://groups.google.com/d/msg/spray-user/8vSTFPQajLM/656ZoXgknv0J
+    val contentTypeAsRawHeader = HttpHeaders.RawHeader("Content-Type", contentTypeString)
+    val parsedContentTypeHeader = HttpParser.parseHeader(contentTypeAsRawHeader)
+
+    // (This match is exhaustive, but it doesn't look so to the compiler)
+    (parsedContentTypeHeader: @unchecked) match {
+      case Right(ct: HttpHeaders.`Content-Type`) =>
+        ct.contentType.mediaType
+      case Left(error: ErrorInfo) =>
+        throw new IllegalArgumentException(
+          s"Error converting '$contentTypeString' to a MediaType: '${error.summary}'")
+    }
   }
   def marshal(v: MediaType): String = v.toString()
 }
